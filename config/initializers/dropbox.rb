c55a80e809922536ff95a35ccd7fe30997358c01
João Vitor Alves <joao.vitor@redealumni.com>
2014-03-31 10:53:55 -0300

    Delete dropbox.rb

--- config/initializers/dropbox.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.dropbox_app_key = "harzunfvtbvaaci"
-  config.dropbox_app_secret = "k7aawpvxfikyg4u"
-  config.dropbox_access_token = "ln2ud3pvnpqx87bc"
-  config.dropbox_access_token_secret = "1bopjseioqn8wz2"
-  config.dropbox_user_id = "162671126"
-  config.dropbox_access_type = "dropbox"
-end
