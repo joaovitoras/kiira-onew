5819c0436b12165ee071ddc3f90dbc2b8149c3ec
João Vitor <joao.vitor@redealumni.com>
2014-03-31 15:32:04 -0300

    static pages...about

--- app/views/drawings/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@drawings) do |drawing|
-  json.extract! drawing, :id, :name, :description
-  json.url drawing_url(drawing, format: :json)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 KiiraOnew::Application.routes.draw do
 
+  get "static_page/who"
+  get "static_page/what"
+  get "static_page/why"
   devise_for :users
   resources :galleries do 
   	resources :drawings
   end
+
+  get '/about/quem',           to: 'static_page#who',  as: :who
+  get '/about/oque',           to: 'static_page#what',  as: :what
+  get '/about/porque',           to: 'static_page#why',  as: :why
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   
   root "galleries#index"
