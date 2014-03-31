9b4fdaa0364bfc20ebebb74a796f7b32ef1f71f4
João Vitor <joao.vitor@redealumni.com>
2014-03-31 11:10:13 -0300

    delete scaffold main

--- app/views/mains/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@mains) do |main|
-  json.extract! main, :id, :heroinfo, :titlenews, :newscontent
-  json.url main_url(main, format: :json)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ KiiraOnew::Application.routes.draw do
   resources :galleries do 
   	resources :drawings
   end
-  resources :mains
-
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   
   root "galleries#index"
