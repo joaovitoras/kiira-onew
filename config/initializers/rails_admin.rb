fa2f905223bc5bf14195a01a7d6ce0e6b1da2bff
João Vitor <joao.vitor@redealumni.com>
2014-06-04 13:37:17 -0300

    new devise pages

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,20 +1,13 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
+  config.main_app_name = ['Kiira', 'Dashboard']
 
   ## == Devise ==
    config.authenticate_with do
      warden.authenticate! scope: :user
    end
+
    config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
   
   config.actions do
     dashboard                     # mandatory
@@ -26,9 +19,44 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
+    history_index
+    history_show    
+  end
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+  config.compact_show_view = false
+
+  config.included_models = ['Gallery']
+
+  config.model 'Gallery' do
+    configure :drawings, :has_many_association
+    configure :name, :string
+    configure :description, :text
+    configure :image
+
+    label 'Galeria'
+    label_plural 'Galerias'   
+    navigation_label 'Art'
+    list do
+      field :name do
+          label 'Nome da Galeria'
+      end
+      field :description do
+          label 'Descrição'
+      end
+      field :image do
+          label 'Imagem da Galerria'
+      end
+    end
+    show do
+      field :name
+      field :description
+      field :image
+    end
+    edit do
+      field :name
+      field :description
+      field :image
+    end
+    export do; end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 KiiraOnew::Application.routes.draw do
 
-
   devise_for :users
+
   resources :galleries do
   	resources :drawings
   end
+  
   get "news",                  to: 'news#index',       as: :news
   get '/about/quem',           to: 'static_page#who',  as: :who
   get '/about/oque',           to: 'static_page#what', as: :what
