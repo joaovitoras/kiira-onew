47a04d49f8e494d5d94b333dee4e18a7a9c4c46e
João Vitor <joao.vitor@redealumni.com>
2014-06-26 13:15:17 -0300

    validates

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,5 +1,12 @@
 class Drawing < ActiveRecord::Base
 	extend FriendlyId
+
+	before_save validates :name, presence: true, uniqueness: true
+	before_save validates :description, presence: true
+	before_save validates :slug, presence: true, uniqueness: true
+	before_save validates :gallery, presence: true
+	before_save validates :image, presence: true
+
 	friendly_id :name, use: :slugged
 	mount_uploader :image, ImageUploader
 	belongs_to :gallery
