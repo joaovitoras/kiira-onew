eabbb62e3760323855106765935b8369b03f76e5
João Vitor <joao.vitor@redealumni.com>
2014-06-26 13:15:17 -0300

    validates

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,12 @@
 class Gallery < ActiveRecord::Base
 	extend FriendlyId
+
+	before_save validates :name, presence: true, uniqueness: true
+	before_save validates :description, presence: true
+	before_save validates :slug, presence: true, uniqueness: true
+	before_save validates :drawings, presence: true
+	before_save validates :image, presence: true
+
 	friendly_id :name, use: :slugged
 	mount_uploader :image, ImageUploader
 	has_many :drawings
