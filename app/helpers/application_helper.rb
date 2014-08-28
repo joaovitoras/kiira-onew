0cf5797e76eeea4f373e802e7cdd871c2405468c
joaovitoras <joaovitoras@vm-0.joaovitoras.koding.kd.io>
2014-08-28 04:52:05 +0000

    adds google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
-
 	def title(page_title)
   		content_for(:title) { page_title }
 	end
+	def google_analytics
+    if Rails.env.development?
+    '<!-- GOOGLE ANALYTICS should be here -->'.html_safe
+
+    '<!-- GOOGLE ANALYTICS should be here -->'.html_safe
+    else
+      render 'partials/google_analytics'
+    end
+  end
 end
