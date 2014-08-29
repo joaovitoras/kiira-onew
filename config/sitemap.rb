4ab6d882805585b0628065f9e0eb42a3c95e19d4
joaovitoras <joaovitoras@vm-0.joaovitoras.koding.kd.io>
2014-08-29 03:41:33 +0000

    delete zicas

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Change this to your host. See the readme at https://github.com/lassebunk/dynamic_sitemaps
 # for examples of multiple hosts and folders.
-host "www.kiiraonew.com.br"
+host "kiiraonew.com.br"
 
 sitemap :site do
   url root_url, last_mod: Time.now, change_freq: "daily", priority: 1.0
@@ -8,4 +8,4 @@ end
 
 sitemap_for Gallery.all
 
-ping_with "http://#{host}/sitemap.xml"
+ping_with "#{host}/sitemap.xml"
