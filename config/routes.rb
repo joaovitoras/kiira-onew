303eedccea211080817db9bfff2390b9613168ba
joaovitoras <joaovitoras@vm-0.joaovitoras.koding.kd.io>
2014-08-29 03:13:02 +0000

    translate routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ KiiraOnew::Application.routes.draw do
 
   devise_for :users
 
-  resources :galleries do
-  	resources :drawings
+  resources :galleries, path: "galerias" do
+  	resources :drawings, path: "desenhos"
   end
 
-  resources :sales do 
-    resources :products
+  resources :sales, path: "vendas" do 
+    resources :products, path: "produtos"
   end
   
   match '/send_mail',          to: 'galleries#send_mail',         via: 'post'
   get 'news',                  to: 'news#index',                  as: :news
-  get '/about/quem',           to: 'static_page#who',             as: :who
-  get '/about/oque',           to: 'static_page#what',            as: :what
-  get '/about/porque',         to: 'static_page#why',             as: :why
+  get '/sobre/quem',           to: 'static_page#who',             as: :who
+  get '/sobre/oque',           to: 'static_page#what',            as: :what
+  get '/sobre/porque',         to: 'static_page#why',             as: :why
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
